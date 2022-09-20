be935128a81b6542b7ffc61b3bdd8b6a61004a70
thanhtuyenne <88424555+thanhtuyenne@users.noreply.github.com>
2022-09-20 12:54:12 +0700

    update move and countclick

---
+++ Assets/Member/Tuyen/CountClick/countClick.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class countClick : MonoBehaviour
+{
+    private Text score;
+    private int scoreAmount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreAmount = 0;
+        score = GetComponent<Text>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score.text = scoreAmount.ToString();
+    }
+
+    public void AddScore()
+    {
+        scoreAmount += 1;
+    }
+}
---
+++ Assets/Member/Tuyen/MOVE/MoveToMouse.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToMouse : MonoBehaviour
+{
+    public float speed = 5f;
+    private Vector3 target;
+    bool toogle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            toogle = !toogle;
+
+        }
+        if (toogle)
+        {
+            target.x = transform.position.x + 1;
+            target.y = transform.position.y;
+            target.z = transform.position.z;
+        }
+        else
+        {
+            target.x = transform.position.x - 1;
+            target.y = transform.position.y;
+            target.z = transform.position.z;
+        }
+        //target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+    }
+}
